66835391f1461a935539b5f2cf37db0ef4865309
cmdlinebeep <joelwigton@gmail.com>
2020-04-24 15:46:09 -0600

    Initial models and migrations set up.  Added favicon, minor form fixes.

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-# DEBUG = True
-# Doing through command line since running with 'flask run'
+# Enable debug mode.  Must run with "python app.py" for this to be picked up
+DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:a@localhost:5432/fyyur_app'
+# toy password for assignment (Windows required it), not real world best practice!
